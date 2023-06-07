936cccdf24aa4f66de6dd87a897fc0ee89079e21
AjithBSMourya <135739987+AjithBSMourya@users.noreply.github.com>
2023-06-07 09:26:17 +0530

    Create euclid.c

---
+++ euclid.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+struct pair {
+    int f;
+    struct pair2 {
+        int f;
+        int s;
+    } s;
+};
+
+struct pair extendedEuclid(int a, int b) 
+{
+    int x = 1, y = 0;
+    int xL = 0, yL = 1;
+    int q, r, m, n;
+    while (a != 0) 
+    {
+        q = b / a;
+        r = b % a;
+        m = xL - q * x;
+        n = yL - q * y;
+        xL = x; 
+        yL = y;
+        x = m; 
+        y = n;
+        b = a; 
+        a = r;
+    }
+    struct pair re;
+    re.f = b;
+    re.s.first = xL;
+    re.s.s = yL;
+    return re;
+}
+
+int modInverse(int a, int m) 
+{
+    return (extendedEuclid(a, m).second.first + m) % m;
+}
+
+int main()
+{
+    int a, m;
+    printf("Enter number to find modular multiplicative inverse: ");
+    scanf("%d", &a);
+    printf("Enter Modular Value: ");
+    scanf("%d", &m);
+    printf("%d\n", modInverse(a, m));
+    return 0;
+}
