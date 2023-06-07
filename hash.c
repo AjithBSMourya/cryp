3b32c5ba49c00970dc4c978e799d4dc013d7a14a
AjithBSMourya <135739987+AjithBSMourya@users.noreply.github.com>
2023-06-07 10:42:52 +0530

    Update hash.c

--- hash.c
+++ hash.c
@@ -10,28 +10,26 @@ unsigned int custom_hash(const char* message) {
 
 int main() {
     char message[100];
-    printf("Enter the message: ");
+    printf("Enter a message: ");
     fgets(message, sizeof(message), stdin);
     message[strcspn(message, "\n")] = '\0'; 
     unsigned int hash_value = custom_hash(message);
 
-    printf("Original Message: %s\n", message);
+    printf("Message: %s\n", message);
     printf("Hash value: %u\n", hash_value);
 
     char received_message[100];
-    printf("Enter the received message: ");
+    printf("Received message: ");
     fgets(received_message, sizeof(received_message), stdin);
     received_message[strcspn(received_message, "\n")] = '\0';
     unsigned int received_hash_value = custom_hash(received_message);
-
-    printf("Received Message: %s\n", received_message);
-    printf("Received Hash value: %u\n", received_hash_value);
+    printf("Hash value: %u\n", received_hash_value);
 
     if (received_hash_value == hash_value) {
-        printf("Integrity: The message has not been modified.\n");
+        printf("Integrity: The message is not modified.\n");
     }
     else {
-        printf("Integrity: The message has been modified.\n");
+        printf("Integrity: The message is modified.\n");
     }
 
     return 0;
